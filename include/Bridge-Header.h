ee096a872f62324417a5448878311a4eddd897ce
linushjeltman <linus.hjeltman@hotmail.com>
2024-05-27 11:39:21 +0200

    Added bridge header and combined libs into one

---
+++ include/Bridge-Header.h
@@ -0,0 +1,32 @@
+//
+//  Bridge-Header.h
+//  testSDK
+//
+//  Created by Linus Hjeltman on 2024-02-13.
+//
+
+#ifndef Bridge_Header_h
+#define Bridge_Header_h
+#import <CBEIOS/CloudBackend.h>
+#import <CBEIOS/Filter.h>
+#import <CBEIOS/QueryResult.h>
+#import <CBEIOS/Container.h>
+#import <CBEIOS/Object.h>
+#import <CBEIOS/Group.h>
+#import <CBEIOS/GroupManager.h>
+#import <CBEIOS/GroupFilter.h>
+#import <CBEIOS/GroupQueryResult.h>
+#import <CBEIOS/Member.h>
+ 
+#import <CBEIOS/Types.h>
+#import <CBEIOS/delegate/Error.h>
+#import <CBEIOS/util/Context.h>
+ 
+#import <CBEIOS/delegate/AclDelegate.h>
+#import <CBEIOS/delegate/UploadDelegate.h>
+#import <CBEIOS/delegate/PublishDelegate.h>
+#import <CBEIOS/delegate/ShareDelegate.h>
+#import <CBEIOS/delegate/UnpublishDelegate.h>
+#import <CBEIOS/delegate/UnShareDelegate.h>
+#import <CBEIOS/delegate/UnsubscribeDelegate.h>
+#endif /* Bridge_Header_h */
